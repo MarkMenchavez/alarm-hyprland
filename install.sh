b49fa195596e441f3a3021eb0905deb0d7b352f9
Mark Menchavez <markchristian.menchavez@gmail.com>
2025-12-13 19:23:16 +0800

    Make script pipe-safe

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 DISK="/dev/nvme0n1"
 
 echo "This will WIPE and repartition ${DISK}"
-read -rp "Type YES to continue: " CONFIRM
+read -rp "Type YES to continue: " CONFIRM </dev/tty
 if [[ "${CONFIRM}" != "YES" ]]; then
   echo "Aborted."
   exit 1
