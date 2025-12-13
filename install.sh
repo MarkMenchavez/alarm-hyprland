618586c683c2ce13801251e940c964d733a12906
Mark Menchavez <markchristian.menchavez@gmail.com>
2025-12-13 19:55:56 +0800

    Remove comments in sfdisk

--- install.sh
+++ install.sh
@@ -60,23 +60,11 @@ sfdisk "${DISK}" <<'EOF'
 label: gpt
 unit: MiB
 first-lba: 2048
-
-# p1 — BIOS boot (reserved / unused)
 size=512,  type=ef02, name="BIOS boot"
-
-# p2 — EFI System Partition
 size=1024, type=ef00, name="EFI System"
-
-# p3 — Linux boot
 size=2048, type=8300, name="Linux boot"
-
-# p4 — Linux swap
 size=8192, type=8200, name="Linux swap"
-
-# p5 — Linux root
 size=40960, type=8300, name="Linux root"
-
-# p6 — Linux home (remaining space)
 type=8300, name="Linux home"
 EOF
 
